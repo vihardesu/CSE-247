d49b9425dd3882769b422c17a351000d99b64722
RonCytron <roncytron@gmail.com>
2016-07-08 15:22:06 -0600

    output working now incrementally

--- coursesupport/timing/examples/Quadratic.java
+++ coursesupport/timing/examples/Quadratic.java
@@ -3,6 +3,7 @@ package timing.examples;
 import timing.GenResults;
 import timing.RepeatRunnable;
 import timing.Ticker;
+import timing.output.Output;
 import timing.quiet.ExecuteQuietAlgorithm;
 import timing.quiet.QuietAlgorithm;
 
@@ -34,12 +35,15 @@ public class Quadratic extends QuietAlgorithm {
 	}
 
 	public static void main(String[] args) {
+		Output out = new Output("quadratic", "quadticks");
 		for (int i=10000; i < 100000; i=i+10000) {
 			ExecuteQuietAlgorithm e = new ExecuteQuietAlgorithm(new Quadratic(i), 3);
 			e.run();
+			out.writeSizeValue(i, e.getTicks());
 			System.out.println(" ticks: " + e.getTicks());
 			System.out.println(" time:  " + e.getTime());
 		}
+		out.close();
 	}
 
 }
--- coursesupport/timing/output/Output.java
+++ coursesupport/timing/output/Output.java
@@ -1,6 +1,7 @@
 package timing.output;
 
 import java.io.FileWriter;
+import java.io.IOException;
 
 import com.csvreader.CsvWriter;
 
@@ -8,37 +9,54 @@ import timing.utils.SizeAndLong;
 
 public class Output {
 
+	private CsvWriter w;
+
+	public Output(String exper, String file) {
+		this(exper,file, false);
+	}
+	public Output(String exper, String file, boolean append) {
+		if (!file.endsWith(".csv")) {
+			file    = file + ".csv";
+		}
+		try {
+			FileWriter fw = new FileWriter("outputs/" + file, append);
+			this.w  = new CsvWriter(fw, ',');
+			w.write("size");
+			w.write(exper);
+			w.endRecord();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Write a series of sizes and times to an output file
 	 * @param file filename to which the output should be appended
 	 * @param exper column name
 	 * @param iter iterator that returns the sizes and durations
 	 */
-	public static void writeSizeTiming(String file, String exper, Iterable<SizeAndLong> iter) {
-		try {
-			if (!file.endsWith(".csv")) {
-				file = file + ".csv";
-			}
-			FileWriter fw = new FileWriter(file, true);
-			CsvWriter w = new CsvWriter(fw, ',');
-			//
-			// Write header row
-			//
-			w.write("size");
-			w.write(exper);
-			w.endRecord();
-			//
-			// and write the data
-			//
-			for (SizeAndLong st : iter) {
-				w.write(""+st.size);
-				w.write(""+st.value);
-				w.endRecord();
-			}
-			w.close();
-		} catch (Throwable t) {
-			System.out.println("Fail write with " + t);
+	public void writeSizeTiming(Iterable<SizeAndLong> iter) {
+		for (SizeAndLong st : iter) {
+			writeSizeValue(st.size, st.value);
 		}
+	}
+
+	public void writeSizeValue(int size, long value) {
+		try {
+			System.out.println("writing " + size + " " + value);
+			w.write("" + size);
+			w.write("" + value);
+			w.endRecord();
+			w.flush();
+		} catch (Throwable t) {
+			throw new Error("oops");
+		}
+
+	}
+	
+	public void close() {
+		w.close();
 	}
 
 }
