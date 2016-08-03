e04f1a4142625d9928418f75cd63b72037348ac5
RonCytron <roncytron@gmail.com>
2016-08-03 11:06:34 -0500

    comments

--- labs/kwaymergesort/KWayMergeSort.java
+++ labs/kwaymergesort/KWayMergeSort.java
@@ -4,18 +4,30 @@ import timing.Ticker;
 
 public class KWayMergeSort {
 	
+	/**
+	 * 
+	 * @param K some positive power of 2.
+	 * @param input an array of unsorted integers whose size is a power of 2 and at least K
+	 * @param ticker call .tick() on this to account for the work you do
+	 * @return
+	 */
 	public static Integer[] kwaymergesort(int K, Integer[] input, Ticker ticker) {
-		Integer[] ans = new Integer[input.length];
+		int n = input.length;
 		
 		//
 		// FIXME
 		// Following just copies the input as the answer
+		//
 		// You must replace the loop below with code that performs
 		// a K-way merge sort, placing the result in ans
-		// Use the ticker as you normally would, to account for
-		// the operations taken to perform the K-way merge sort
 		//
-		for (int i=0; i < input.length; ++i) {
+		// The web page for this assignment provides more detail.
+		//
+		// Use the ticker as you normally would, to account for
+		// the operations taken to perform the K-way merge sort.
+		//
+		Integer[] ans = new Integer[n];
+		for (int i=0; i < n; ++i) {
 			ans[i] = input[i];
 			ticker.tick();
 		}
