b8893fab483c5d89bd4fefc692a62df6c0073c14
RonCytron <roncytron@gmail.com>
2016-08-03 10:16:23 -0500

    comments

--- labs/kwaymergesort/KWayMergeSort.java
+++ labs/kwaymergesort/KWayMergeSort.java
@@ -11,7 +11,9 @@ public class KWayMergeSort {
 		// FIXME
 		// Following just copies the input as the answer
 		// You must replace the loop below with code that performs
-		// a K-way mergesort
+		// a K-way merge sort, placing the result in ans
+		// Use the ticker as you normally would, to account for
+		// the operations taken to perform the K-way merge sort
 		//
 		for (int i=0; i < input.length; ++i) {
 			ans[i] = input[i];
